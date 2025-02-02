a82413a7a5372d20bad95a814194f189cbfa0c55
ahmvdev <ahmadsaeed3290@gmail.com>
2025-02-02 15:30:19 +0500

    uploading

---
+++ src/components/Form.jsx
@@ -0,0 +1,10 @@
+export default function () {
+    return (
+        <>
+            <div className="flex justify-center pt-20">
+                <input placeholder="e.g oregano" className="p-2 rounded-md mr-4"></input>
+                <button className="bg-black rounded-md p-3 text-white hover:cursor-pointer">+ Add Ingredient</button>
+            </div>
+        </>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function () {
+    return (
+        <>
+            <div className="flex items-center justify-center">
+                <img src="/chef.png" className="w-16"></img>
+                <h1 className="text-2xl font-bold">Chef Claude</h1>
+            </div>
+        </>
+    )
+}
